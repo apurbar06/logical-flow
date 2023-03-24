acb38f948af39dc5e84fc6d2e4bdf4f4e8c3ba12
apurbar06 <apurbar06@gmail.com>
2023-03-24 20:42:50 +0530

    add functionality for adding new argument dinamically

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [args, setArgs] = useState([
+    {
+      name: "myArg",
+      value: true,
+    },
+  ]);
+
+  const addNewArg = () => {
+    setArgs([
+      ...args,
+      {
+        name: "",
+        value: false,
+      },
+    ]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {args.map((arg, index) => (
+        <div key={index}>
+          <input
+            type="text"
+            value={arg.name}
+            onChange={(e) => {
+              const val = e.target.value;
+              const newArgs = args.map((item, i) =>
+                i === index ? { ...item, name: val } : item
+              );
+              setArgs(newArgs);
+            }}
+          ></input>
+          <select
+            value={arg.value}
+            onChange={(e) => {
+              const val = e.target.value === "true" ? true : false;
+              const newArgs = args.map((item, i) =>
+                i === index ? { ...item, value: val } : item
+              );
+              setArgs(newArgs);
+            }}
+          >
+            <option value="true">true</option>
+            <option value="false">false</option>
+          </select>
+        </div>
+      ))}
+      <button onClick={addNewArg}>Add Arg</button>
+
     </div>
   );
 }
-
 
 export default App;
